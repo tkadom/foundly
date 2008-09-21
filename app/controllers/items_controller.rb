628fc57f43fbe3b19c8f33759e8a0d5940c3a770
Tim Kadom <tkadom@skiptree.com>
2008-09-21 18:45:42 -0400

    adds search and initial links to home page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,26 +2,13 @@ require 'tiny_mce'
 class ItemsController < ApplicationController
   layout 'application'
 
-   uses_tiny_mce(:options => {  
-        :theme => 'advanced',  
-        :mode => "textareas",  
-        :height => 100,  
-        :content_css => "/stylesheets/base.css",  
-        :remove_script_host => true,  
-        :theme_advanced_toolbar_location => "top",  
-        :theme_advanced_toolbar_align => "left",  
-        :theme_advanced_buttons2 => %w{ spellchecker },  
-        :editor_selector => 'mceEditor',  
-        :spellchecker_rpc_url => "/items/spellchecker",  
-        :spellchecker_languages => "+English=en",  
-        :plugins => %w{ contextmenu paste spellchecker}  
-         },  
-        :only => [:new])  
-
   # GET /items
   # GET /items.xml
   def index
-    @items = Item.paginate :page => params[:page], :order => 'created_at DESC'
+    @items = Item.paginate (:all,
+                            :page => params[:page], 
+                            :order => 'created_at DESC',
+                            :conditions => search_conditions )
 
     respond_to do |format|
       format.html # index.html.erb
@@ -110,4 +97,13 @@ class ItemsController < ApplicationController
      render :text => results.to_json  
      return  
    end  
+  protected 
+
+  def search_conditions
+    if params[:q].blank?
+      return nil
+    else
+      return "items.description like '%#{params[:q]}%'"
+    end
+  end
 end
